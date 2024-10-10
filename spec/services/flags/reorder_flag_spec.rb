c2b681e3688658a10c584544af44c65cac7e5f22
Loïc Guitaut <loic@discourse.org>
2024-10-10 18:02:36 +0200

    WIP: some specs refactor

--- spec/services/flags/reorder_flag_spec.rb
+++ spec/services/flags/reorder_flag_spec.rb
@@ -1,52 +1,68 @@
 # frozen_string_literal: true
 
 RSpec.describe(Flags::ReorderFlag) do
-  subject(:result) { described_class.call(params:, **dependencies) }
-
-  fab!(:current_user) { Fabricate(:admin) }
-
-  let(:params) { { flag_id: flag.id, direction: } }
-  let(:dependencies) { { guardian: current_user.guardian } }
-  let(:flag) { Flag.order(:position).last }
-  let(:direction) { "up" }
-
-  context "when user is not allowed to perform the action" do
-    fab!(:current_user) { Fabricate(:user) }
-
-    it { is_expected.to fail_a_policy(:invalid_access) }
+  describe described_class::Contract, type: :model do
+    it { is_expected.to validate_presence_of(:flag_id) }
+    it { is_expected.to validate_inclusion_of(:direction).in_array(%w[up down]) }
   end
 
-  context "when direction is invalid" do
-    let(:direction) { "side" }
+  describe ".call" do
+    subject(:result) { described_class.call(params:, **dependencies) }
 
-    it { is_expected.to fail_a_contract }
-  end
+    fab!(:current_user) { Fabricate(:admin) }
 
-  context "when move is invalid" do
-    let(:direction) { "down" }
+    let(:params) { { flag_id: flag_id, direction: } }
+    let(:dependencies) { { guardian: current_user.guardian } }
+    let(:flag_id) { flag.id }
+    let(:flag) { Flag.order(:position).last }
+    let(:direction) { "up" }
 
-    it { is_expected.to fail_a_policy(:invalid_move) }
-  end
+    context "when contract is invalid" do
+      let(:direction) { "left" }
 
-  context "when user is allowed to perform the action" do
-    it { is_expected.to run_successfully }
-
-    it "moves the flag" do
-      expect(Flag.order(:position).map(&:name)).to eq(
-        %w[notify_user off_topic inappropriate spam illegal notify_moderators],
-      )
-      result
-      expect(Flag.order(:position).map(&:name)).to eq(
-        %w[notify_user off_topic inappropriate spam notify_moderators illegal],
-      )
+      it { is_expected.to fail_a_contract }
     end
 
-    it "logs the action" do
-      expect { result }.to change { UserHistory.count }.by(1)
-      expect(UserHistory.last).to have_attributes(
-        custom_type: "move_flag",
-        details: "flag: #{result[:flag].name}\ndirection: up",
-      )
+    context "when contract is valid" do
+      context "when model is not found" do
+        let(:flag_id) { 0 }
+
+        it { is_expected.to fail_to_find_a_model(:flag) }
+      end
+
+      context "when model exists" do
+        context "when user is not allowed to perform the action" do
+          fab!(:current_user) { Fabricate(:user) }
+
+          it { is_expected.to fail_a_policy(:invalid_access) }
+        end
+
+        context "when user is allowed to perform the action" do
+          context "when move is invalid" do
+            let(:direction) { "down" }
+
+            it { is_expected.to fail_a_policy(:invalid_move) }
+          end
+
+          context "when move is valid" do
+            it { is_expected.to run_successfully }
+
+            it "moves the flag" do
+              expect { result }.to change { Flag.order(:position).map(&:name) }.from(
+                %w[notify_user off_topic inappropriate spam illegal notify_moderators],
+              ).to(%w[notify_user off_topic inappropriate spam notify_moderators illegal])
+            end
+
+            it "logs the action" do
+              expect { result }.to change { UserHistory.count }.by(1)
+              expect(UserHistory.last).to have_attributes(
+                custom_type: "move_flag",
+                details: "flag: #{result[:flag].name}\ndirection: up",
+              )
+            end
+          end
+        end
+      end
     end
   end
 end
